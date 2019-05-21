6859ab7b874155da6bc50062296ceeca2c81f351
Sascha <sascha.majewsky93@gmx.de>
2019-05-22 00:05:39 +0200

    added in images for the feature.html, added in a tablet and mobile burger navbar with animations and class toggles through javascript

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    // toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // animate nav links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else { 
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        // animate burger
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
