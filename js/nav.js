b442e2668cb3eb4191bbd4bd6af9e913e8d51c36
Sascha <sascha.majewsky93@gmx.de>
2019-05-22 03:34:05 +0200

    added a blur effect when burger menu is open

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links a');
+    const mainSection = document.querySelector('.home-main-section');
 
     // toggle nav
     burger.addEventListener('click', () => {
@@ -11,8 +12,10 @@ const navSlide = () => {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
+                mainSection.classList.remove('blur-content');
             } else { 
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                mainSection.classList.add('blur-content');
             }
         });
         // animate burger
